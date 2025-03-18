50c6a0935bbb33052f47e5c4c4c5a77a3b23a98c
FelixAnthonisen <felix.anthonisen@gmail.com>
2025-03-18 15:19:56 +0100

    update conv network

--- src/models/localization/cnn_network_1.py
+++ src/models/localization/cnn_network_1.py
@@ -11,22 +11,24 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.convolutional_layers = nn.Sequential(
             nn.Conv2d(
-                in_channels=1, out_channels=16, kernel_size=3, padding=1
+                in_channels=1, out_channels=8, kernel_size=3, padding=1
             ),  # (N, 1, 48, 60) -> (N, 16, 48, 60)
             nn.ReLU(),  # (N, 16, 48, 60)
             nn.MaxPool2d(kernel_size=2, stride=2),  # (N, 16, 48, 60) -> (N, 16, 24, 30)
             nn.Conv2d(
-                in_channels=16, out_channels=32, kernel_size=3, padding=1
+                in_channels=8, out_channels=16, kernel_size=3, padding=1
             ),  # (N, 16, 24, 30) -> (N, 32, 24, 30)
             nn.ReLU(),  # (N, 32, 24, 30)
             nn.MaxPool2d(kernel_size=2, stride=2),  # (N, 32, 24, 30) -> (N, 32, 12, 15)
         )
 
         self.fully_connected_layers = nn.Sequential(
-            nn.Linear(32 * 12 * 15, 64),  # (N, 32*3*3) -> (N, 164)
+            nn.Linear(16 * 12 * 15, 32),
             nn.ReLU(),
-            nn.Dropout(0.25),  # (N, 164) -> (N, 164)
-            nn.Linear(64, num_classes),  # (N, 64) -> (N, num_classes)
+            nn.Dropout(0.25),
+            nn.Linear(32, 24),
+            nn.ReLU(),
+            nn.Linear(24, num_classes),
         )
 
     def forward(self, x):
