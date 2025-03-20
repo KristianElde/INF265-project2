41a863faf97aca432d6c6f5a9f1bc3d08d006926
FelixAnthonisen <felix.anthonisen@gmail.com>
2025-03-20 16:09:00 +0100

    fix conv layers

--- src/models/localization/cnn_network_1.py
+++ src/models/localization/cnn_network_1.py
@@ -19,12 +19,12 @@ class CNN(nn.Module):
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=3),
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
             nn.ReLU(),
         )
 
         self.fully_connected_layers = nn.Sequential(
-            nn.Linear(256 * 4 * 5, 128),
+            nn.Linear(128 * 4 * 5, 128),
             nn.ReLU(),
             nn.Dropout(0.25),
             nn.Linear(128, 64),
