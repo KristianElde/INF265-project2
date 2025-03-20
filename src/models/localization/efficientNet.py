197cdaa4da2eb85e247c389a7eff3b06c4ee4d87
Kristian Elde Johansen <kristian.e.johansen@student.uib.no>
2025-03-20 17:50:05 +0100

    make efficientnet take 1 channel input

--- src/models/localization/efficientNet.py
+++ src/models/localization/efficientNet.py
@@ -7,8 +7,22 @@ class EfficientNetLocalization(nn.Module):
     def __init__(self, num_classes=10):
         super().__init__()
         efficientnet = models.efficientnet_b0(pretrained=False)
+
+        first_conv = efficientnet.features[0][0]
+        new_conv = nn.Conv2d(
+            in_channels=1,  # Change from 3 to 1
+            out_channels=first_conv.out_channels,
+            kernel_size=first_conv.kernel_size,
+            stride=first_conv.stride,
+            padding=first_conv.padding,
+            bias=first_conv.bias is not None,
+        )
+
+        # Replace the first layer
+        efficientnet.features[0][0] = new_conv
+
         self.backbone = efficientnet.features
-        self.fc = nn.Linear(1280, 1 + 4 + num_classes)
+        self.fc = nn.Linear(5120, 1 + 4 + num_classes)
 
     def forward(self, x):
         x = self.backbone(x)
