1e3bfd0a704337336e8e46bdc641c64693f09c53
FelixAnthonisen <felix.anthonisen@gmail.com>
2025-03-21 15:41:48 +0100

    change detection 1 network

--- src/models/detection/cnn_network.py
+++ src/models/detection/cnn_network.py
@@ -1,32 +1,36 @@
 import torch.nn as nn
+import torch
 
 
 class CNN(nn.Module):
-    def __init__(self, num_classes=7):
+    def __init__(
+        self,
+        num_classes=7,
+    ):
         super(CNN, self).__init__()
         self.convolutional_layers = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=3, padding=1),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(16, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
+            nn.MaxPool2d(kernel_size=3, stride=3),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(3, 3),
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
+        )
+
+        self.fully_connected_layers = nn.Sequential(
+            nn.Linear(128 * 4 * 5, 128),
             nn.ReLU(),
-            nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Conv2d(256, 2 * 3 * num_classes, kernel_size=1),
+            nn.Linear(128, 2 * 3 * num_classes),
         )
 
     def forward(self, x):
         x = self.convolutional_layers(x)
+        x = torch.flatten(x, 1)
+        x = self.fully_connected_layers(x)
         x = x.view(x.shape[0], 2, 3, 7)
         return x
