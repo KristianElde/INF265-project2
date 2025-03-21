2519ed92dbf6adf917c97be219951967bc510da0
FelixAnthonisen <felix.anthonisen@gmail.com>
2025-03-21 12:35:40 +0100

    make network good again

--- src/models/detection/cnn_network.py
+++ src/models/detection/cnn_network.py
@@ -5,26 +5,28 @@ class CNN(nn.Module):
     def __init__(self, num_classes=7):
         super(CNN, self).__init__()
         self.convolutional_layers = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=2, kernel_size=3, padding=1),
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Conv2d(in_channels=2, out_channels=3, kernel_size=3, padding=1),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=3, out_channels=4, kernel_size=3, padding=1),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.Conv2d(in_channels=4, out_channels=5, kernel_size=3, padding=1),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=5, out_channels=6, kernel_size=3, padding=1),
+            nn.MaxPool2d(3, 3),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(5, 4), stride=(1, 1)),
-            nn.Conv2d(
-                in_channels=6, out_channels=num_classes, kernel_size=3, padding=1
-            ),
-            nn.MaxPool2d(kernel_size=4, stride=4),
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Conv2d(512, 2 * 3 * num_classes, kernel_size=1),
         )
 
     def forward(self, x):
         x = self.convolutional_layers(x)
-        x = x.permute(0, 2, 3, 1)
+        x = x.view(x.shape[0], 2, 3, 7)
         return x
