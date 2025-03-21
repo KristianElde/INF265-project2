4e20367d18201acbc5e05a162a7200073b59e630
FelixAnthonisen <felix.anthonisen@gmail.com>
2025-03-21 13:59:05 +0100

    decrease number of channels

--- src/models/detection/cnn_network.py
+++ src/models/detection/cnn_network.py
@@ -5,25 +5,25 @@ class CNN(nn.Module):
     def __init__(self, num_classes=7):
         super(CNN, self).__init__()
         self.convolutional_layers = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.Conv2d(1, 16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
+            nn.MaxPool2d(2, 2),
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2),
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
+            nn.MaxPool2d(3, 3),
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.MaxPool2d(3, 3),
-            nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
             nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Conv2d(512, 2 * 3 * num_classes, kernel_size=1),
+            nn.Conv2d(256, 2 * 3 * num_classes, kernel_size=1),
         )
 
     def forward(self, x):
