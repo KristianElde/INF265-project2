ed2ac1ecd82d9438ed6fea6dcc7bccff1e93d790
FelixAnthonisen <felix.anthonisen@gmail.com>
2025-03-21 09:14:58 +0100

    clean up

--- src/models/localization/cnn_network_1.py
+++ src/models/localization/cnn_network_1.py
@@ -2,13 +2,13 @@ import torch
 import torch.nn as nn
 
 
-class CNN(nn.Module):
+class CNN1(nn.Module):
     def __init__(
         self,
         num_classes=15,
     ):
         torch.manual_seed(42)
-        super(CNN, self).__init__()
+        super(CNN1, self).__init__()
         self.convolutional_layers = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, padding=1),
             nn.ReLU(),
